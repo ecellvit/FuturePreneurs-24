417765b6c93d3bbcedd1ea50eef254aae9fdce87
KV225511 <krissh.verma2023@vitstudent.ac.in>
2024-09-16 13:35:39 +0530

    join and create team ui updated

--- app/join&createTeam/page.js
+++ app/join&createTeam/page.js
@@ -14,7 +14,7 @@ export default function page() {
             <h1 className="text-center text-[4vh] py-5 text-[#FFFFFF]">Join your team</h1>
             <div className="flex flex-col  items-center gap-5">
               <input type="text" placeholder="Enter team name" className=" sm:landscape:w-[20vw] sm:landscape:h-[7vh] md:max-w-[40vw] portrait:lg:w-[30vw] portrait:lg:text-2xl lg:w-[15vw] w-[55vw] h-[5vh] rounded-md text-xl text-slate-900  focus:outline-none focus:placeholder-transparent active:scale-95 transition-all duration-300"/>
-              <button type="Submit" className=" sm:landscape:w-[15vw]  mb-7 rounded-3xl bg-gradient-to-r from-[#03A3FE] to-[#00FFA3] text-center portrait:lg:w-[30vw]  md:max-w-[25vw] md:text-[20px] landscape:md:text-[1.6vh] lg:w-[15vw] w-[50vw] h-[5vh] hover:scale-110 active:scale-95 transition-transform ease-in-out duration-300 ">Find team to join</button>
+              <button type="Submit" className=" sm:landscape:w-[15vw]  mb-7 rounded-3xl bg-gradient-to-r from-[#03A3FE] to-[#00FFA3] text-center portrait:lg:w-[30vw]  md:max-w-[25vw] md:text-[20px] landscape:md:text-[1.6vh] lg:w-[15vw] w-[50vw] h-[5vh] hover:scale-110 active:scale-95 transition-transform ease-in-out duration-300 text-[#FFFFFF]">Find team to join</button>
             </div>
           </div>
           <div className="lg:h-[30vh] lg:w-[0vw] w-[70vw] border-2 border-[#D9D9D9] rounded-lg "></div>
@@ -22,7 +22,7 @@ export default function page() {
             <h1 className="text-center text-[4vh] py-5 text-[#FFFFFF]">Create your team</h1>
             <div className="flex flex-col justify-evenly items-center gap-5">
               <input type="text" placeholder="Enter team name" className=" sm:landscape:w-[20vw] sm:landscape:h-[7vh] md:max-w-[40vw] portrait:md:max-w-[40vw] portrait:lg:w-[30vw] portrait:lg:text-2xl lg:w-[15vw] w-[55vw] h-[5vh] rounded-md text-xl text-slate-900  focus:outline-none focus:placeholder-transparent active:scale-95 transition-all duration-300"/>
-              <button type="Submit" className="mb-7 sm:landscape:w-[15vw]  rounded-3xl bg-gradient-to-r from-[#03A3FE] to-[#00FFA3] text-center portrait:lg:w-[30vw]  md:max-w-[25vw] md:text-[1.6vh]  sm:landscape:md:text-[1.7vh] lg:w-[15vw] w-[50vw] h-[5vh] hover:scale-110 active:scale-95 transition-transform ease-in-out duration-300">Create your own team</button>
+              <button type="Submit" className="mb-7 sm:landscape:w-[15vw]  rounded-3xl bg-gradient-to-r from-[#03A3FE] to-[#00FFA3] text-center portrait:lg:w-[30vw]  md:max-w-[25vw] md:text-[1.6vh]  sm:landscape:md:text-[1.7vh] lg:w-[15vw] w-[50vw] h-[5vh] hover:scale-110 active:scale-95 transition-transform ease-in-out duration-300 text-[#FFFFFF]">Create your own team</button>
             </div>
           </div>
         </div>
